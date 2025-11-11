ab7b261d2e4fd36e2815539cc847a249d58f783b
Marwan Mohamed <marawan200311@gmail.com>
2025-11-11 14:15:50 +0200

    here I added new page for every button

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template ,url_for
 
 app= Flask(__name__)
-
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/add_page')
+def add_page():
+    return render_template('add.html')  
+
+
+@app.route('/delete_page') 
+def delete_page():
+    return render_template('delete.html')
+@app.route('/update_page')
+def update_index():
+    return render_template('update.html')
+
+@app.route('/show_page')
+def show_page():
+    return render_template('update.html')
+
+
 
 if __name__=="__main__":
       app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,47 @@
+const addButton = document.querySelector('.Add');
+
+
+addButton.addEventListener('click', () => {
+    
+    window.open('/add_page', '_blank'); 
+});
+
+
+
+const deletButton= document.querySelector('.Delete');
+
+deletButton.addEventListener('click' , () => {
+window.open('/delete_page','_blank')
+}
+
+);
+
+
+
+
+
+
+
+
+const ubdateButton = document.querySelector('.Update')
+ubdateButton.addEventListener('click', ()=> {
+window.open('/update_page','_blank')
+
+});
+
+
+
+
+
+
+
+
+const showButton = document.querySelector('.Show')
+showButton.addEventListener('click',()=>{
+window.open('/show_page','blank')
+});
+
+
+
+
+
